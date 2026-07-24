a10e92417273776ede17879a2358c7bf8564ad9f
selbyheyman <104396572+selbyheyman@users.noreply.github.com>
2026-07-24 15:31:48 -0400

    Create writeups.js

---
+++ assets/js/writeups.js
@@ -0,0 +1,74 @@
+async function loadProjects() {
+
+    try {
+
+        const response = await fetch("assets/data/posts.json");
+
+        if (!response.ok) {
+            throw new Error("Unable to load posts.json");
+        }
+
+
+        const posts = await response.json();
+
+
+        const projects = posts.filter(post =>
+            post.type === "project"
+        );
+
+
+        projects.forEach(project => {
+
+
+            const card = createPostCard(project);
+
+
+            switch(project.category) {
+
+
+                case "minecraft":
+
+                    document.getElementById(
+                        "minecraft-projects"
+                    ).innerHTML += card;
+
+                    break;
+
+
+
+                case "unity":
+
+                    document.getElementById(
+                        "unity-projects"
+                    ).innerHTML += card;
+
+                    break;
+
+
+
+                case "homelab":
+
+                    document.getElementById(
+                        "homelab-projects"
+                    ).innerHTML += card;
+
+                    break;
+
+            }
+
+        });
+
+
+    } catch(err) {
+
+        console.error(err);
+
+    }
+
+}
+
+
+document.addEventListener(
+    "DOMContentLoaded",
+    loadProjects
+);
