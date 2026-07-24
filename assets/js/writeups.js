38e1003a582b46b58e4d4947f0d59c8ba33a7e82
selbyheyman <104396572+selbyheyman@users.noreply.github.com>
2026-07-24 16:04:01 -0400

    Update writeups.js

--- assets/js/writeups.js
+++ assets/js/writeups.js
@@ -1,7 +1,6 @@
 async function loadWriteups() {
-
+    
     try {
-
         const response = await fetch("assets/data/posts.json");
 
         if (!response.ok) {
@@ -10,41 +9,28 @@ async function loadWriteups() {
 
         const posts = await response.json();
 
-
         const writeups = posts.filter(post =>
             post.type === "writeup"
         );
 
-
         writeups.forEach(writeup => {
-
             const card = createPostCard(writeup);
 
+            switch(writeup.category) {
+                case "ctf":
+                    document.getElementById("ctf-writeups").innerHTML += card;
+                    break;
 
-            if (writeup.category === "ctf") {
-
-                document.getElementById("ctf-writeups").innerHTML += card;
-
+                case "bug-bounty":
+                    document.getElementById("bug-bounty-writeups").innerHTML += card;
+                    break;
             }
-
-
-            if (writeup.category === "bug-bounty") {
-
-                document.getElementById("bug-bounty-writeups").innerHTML += card;
-
-            }
-
         });
 
-
     } catch (err) {
-
         console.error(err);
-
     }
-
 }
-
 
 document.addEventListener(
     "DOMContentLoaded",
